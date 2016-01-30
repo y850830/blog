a0555812b7575fe523326fe4b6fd136227fffc14
y850830 <40343236@gm.nfu.edu.tw>
2016-01-30 10:59:37 +0800

    add modle seeder

---
+++ database/migrations/2016_01_30_023730_create_blogs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('string');
+            $table->text('text',255);
+            $table->boolean("is_feature");
+            $table->integer('page_view');
+            $table->timestamps('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('blogs');
+    }
+}
